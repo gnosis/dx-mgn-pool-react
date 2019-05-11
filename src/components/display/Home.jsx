701677b6f2ed5e2a23016dcee879586bb32bc8ec
David <david.sato64@gmail.com>
2019-05-11 00:16:12 -0700

    Home connected to HOCs

--- src/components/display/Home.jsx
+++ src/components/display/Home.jsx
@@ -4,6 +4,10 @@ import PoolData from './PoolData'
 import UserStateDisplay from './UserStateDisplay'
 import Footer from './Footer'
 
+import { connect } from '../StateProvider'
+
+import { withAPIConnect, withConfigDisplay, withModal, withPoolSwitching, withWalletConnect } from '../hoc'
+
 const Home = () => (
   <section className="Home">
     {/* Show User's State */}
@@ -19,4 +23,34 @@ const Home = () => (
   </section>
 )
 
-export default Home
+const mapProps = ({
+  // state properties
+  state: {
+    PROVIDER: { ACTIVE_PROVIDER },
+    SHOW_MODAL,
+  },
+  // DISPATCH FUNCTIONS
+
+  // DxMgnPool
+  setPoolTokenInfo,
+  // Provider
+  registerProviders,
+  setActiveProvider,
+  // App
+  showModal,
+}) => ({
+  // state properties
+  state: {
+    ACTIVE_PROVIDER,
+    SHOW_MODAL,
+  },
+  // dispatchers
+  dispatchers: {
+    registerProviders,
+    setActiveProvider,
+    showModal,
+    setPoolTokenInfo,
+  },
+})
+
+export default connect(mapProps)(withModal(withWalletConnect(withPoolSwitching(withAPIConnect(process.env.SHOW_APP_DATA ? withConfigDisplay(Home) : Home)))))
