a88764bc70c964de9d6a53b1685a83ea55630b9c
David <david.sato64@gmail.com>
2019-05-11 00:03:31 -0700

    withModal HOC

--- src/components/hoc/withModal.jsx
+++ src/components/hoc/withModal.jsx
@@ -5,11 +5,13 @@ import LoadingModal from '../display/Modals/LoadingModal'
  * Configuration Displayer HOC
  * @param {*} Component Component to wrap
  */
-const ModalHOC = Component =>
+export const withModal = Component =>
   props => 
-    <>
-      {props.state.SHOW_MODAL && <LoadingModal header={props.state.SHOW_MODAL} />}
-      <Component {...props} />
-    </>
+    (
+      <>
+        {props.state.SHOW_MODAL && <LoadingModal header={props.state.SHOW_MODAL} /* showOrHide={props.state.SHOW_MODAL}  *//>}
+        <Component {...props} />
+      </>
+    )
 
-export default ModalHOC
+export default withModal
