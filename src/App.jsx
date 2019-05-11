87c4a47e499ac751a1730c4804fef9dd9072c60f
David <david.sato64@gmail.com>
2019-05-11 00:16:31 -0700

    connected modal to main App and cleanup App code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-import { DutchXVerificationHOC } from '@gnosis.pm/dutchx-verification-react'
+import { DutchXVerificationHOC as withDXVerification } from '@gnosis.pm/dutchx-verification-react'
 
 import AppOnlineStatusBar from './components/display/AppOnlineStatus'
 import Blocked from './components/display/Modals/Blocked'
 import Home from './components/display/Home'
 import StateProvider from './components/StateProvider'
-import WalletIntegration from './components/controls/WalletIntegration'
-
-import { LOCALFORAGE_KEYS } from './globals'
 
 import { 
   GlobalSubscription,
   GlobalSub,
 } from './subscriptions'
 
-const ConnectedApp = () =>
+import { LOCALFORAGE_KEYS } from './globals'
+
+const SubscribedApp = () =>
   // App is NOT blocked - render
   <GlobalSubscription source={GlobalSub}>
     {subState =>
       <StateProvider subState={subState}>       
         <AppOnlineStatusBar />
-        <WalletIntegration>
-          <Home />
-        </WalletIntegration>
+        <Home />
       </StateProvider>
     }
   </GlobalSubscription>
 
-const ModalWrappedApp = DutchXVerificationHOC(ConnectedApp)(LOCALFORAGE_KEYS.VERIFICATION_SETTINGS, LOCALFORAGE_KEYS.COOKIE_SETTINGS)
+const ModalWrappedApp = withDXVerification(SubscribedApp)(LOCALFORAGE_KEYS.VERIFICATION_SETTINGS, LOCALFORAGE_KEYS.COOKIE_SETTINGS)
 
 const App = ({
   disabledReason,
