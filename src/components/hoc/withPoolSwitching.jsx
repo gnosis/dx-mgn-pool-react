6fc54d809603c1d5095aa3baee71e3057f475206
David <david.sato64@gmail.com>
2019-05-11 00:03:22 -0700

    withPoolSwitcher HOC

--- src/components/hoc/withPoolSwitching.jsx
+++ src/components/hoc/withPoolSwitching.jsx
@@ -1,11 +1,24 @@
-import React, { useEffect, useState } from 'react'
+/* eslint-disable no-debugger */
+import React, { useLayoutEffect, useState } from 'react'
+
+import DataDisplayVisualContainer from '../display/DataDisplay'    
 
 const MockCoordData = [
     {
-        tokenA: 'REAL',
-        tokenB: 'TOKEN',
+        tokenA: 'WETH',
+        tokenB: 'GNO',
         1: {
             address: '0xD4D3e3Ea73a7370e8e0A0cb5FD24DC13DC1668f4',
+        },
+        4: {
+            address: '0x9878669e883DDfC52620a6493b361213444cB974',
+        },
+    },
+    {
+        tokenA: 'WETH [old]',
+        tokenB: 'GNO [old]',
+        1: {
+            address: '0xE1d2ce6F25Cd8Ef4d02de2d268A9e52b62659E59',
         },
         4: {
             address: '0x9878669e883DDfC52620a6493b361213444cB974',
@@ -13,69 +26,73 @@ const MockCoordData = [
     },
     {
         tokenA: 'WETH',
-        tokenB: 'GOGO',
+        tokenB: 'RDN',
         1: {
-            address: '0xD4D3e3Ea73a7370e8e0A0cb5FD24DC13DC1668f4',
-        },
-        4: {
-            address: '0x9878669e883DDfC52620a6493b361213444cB974',
-        },
-    },
-    {
-        tokenA: 'TEEF',
-        tokenB: 'MILK',
-        1: {
-            address: '0xD4D3e3Ea73a7370e8e0A0cb5FD24DC13DC1668f4',
-        },
-        4: {
-            address: '0x9878669e883DDfC52620a6493b361213444cB974',
-        },
-    },
-    {
-        tokenA: 'BEEF',
-        tokenB: 'LMAO',
-        1: {
-            address: '0xD4D3e3Ea73a7370e8e0A0cb5FD24DC13DC1668f4',
+            address: '0xf83fF988570d9fb37389DC84a65d09BCF05A4569',
         },
         4: {
             address: '0x9878669e883DDfC52620a6493b361213444cB974',
         },
     },
 ]
-
 const fakePromise = async time => new Promise(accept => setTimeout(() => accept(MockCoordData), time))
 
 const PoolPicker = ({
+    currentPool,
     handlePoolSelect,
     pools,
-}) => {
-    console.debug('POOLS = ', pools)
-    return (
-        <div className="poolSwitcherContainer">
-            <h3 style={{ width: '100%', textAlign: 'center' }}>available pools:</h3>
-            {pools.map(({ coordinator, tokenA, tokenB }) => 
-                <div 
-                    style={{ minWidth: '50%' }}
-                    onClick={() => handlePoolSelect(coordinator)}
-                >
-                    <p>{coordinator.toLowerCase()}</p>
-                    <p>{tokenA.toLowerCase()}-{tokenB.toLowerCase()}</p>
-                </div>)}
-        </div>
-    )
-}
+    startOpen,
+}) => ( 
+    <section className="Home" style={{ height: '68%' }}>
+        <DataDisplayVisualContainer
+            title="available pools"
+            colour="green"
+            customStyle={{
+                display: 'flex',
+                flexFlow: 'column nowrap',
+                width: "100%",
+            }}
+            preformatted={false}
+            startOpen={startOpen}
+            showOverflow={false}
+            transition={false}
+        >
+            {() =>
+                <>
+                    {currentPool && <h6 style={{ background: '#d0ffeb', textAlign: 'center', padding: 10, margin: 0 }}>current pool: {currentPool.toLowerCase()}</h6>}
+                    <div className="poolSwitcherContainer">
+                        {pools.map(({ 1: { address: coordinator }, tokenA, tokenB }) => {
+                            // don't show current selected pool
+                            if (coordinator === currentPool) return null
 
-const withPoolSwitching = WrappedComponent =>
-    function PoolSwitcher(props) {
+                            return (
+                                <div 
+                                    className="poolSwitcherPool"
+                                    key={`${coordinator}-${Math.random()}`}
+                                    style={{ minWidth: '50%' }}
+                                    onClick={() => handlePoolSelect(coordinator)}
+                                >
+                                    <p>{coordinator.toLowerCase()}</p>
+                                    <p>{tokenA.toLowerCase()}-{tokenB.toLowerCase()}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </>}
+        </DataDisplayVisualContainer>
+    </section>
+)
+
+export const withPoolSwitching = WrappedComponent =>
+    function PoolSwitchHOC(props) {
+        const { dispatchers: { showModal } } = props
+        
         const [error, setError] =               useState(undefined)
         const [pools, setPools] =               useState([])
-        const [loading, setLoading] =           useState(false)
         const [poolSelected, setPoolSelected] = useState(undefined)
-
+        
         // Mount
-        useEffect(() => {
-            setLoading(true)
-
+        useLayoutEffect(() => {
             // grab data async?
             const grabPools = async () => {
                 try {
@@ -87,20 +104,25 @@ const withPoolSwitching = WrappedComponent =>
                 } catch (err) {
                     console.error(err)
                     setError(err)
-                } finally {
-                    setLoading(false)
                 }
             }
             
+            // Start logic
+            showModal('loading available pools')
             grabPools()
+            .then(() => showModal(null))
         }, [])
-
-        if (loading) return <div className="poolSwitcherContainer"><h1 style={{ margin: 'auto', textAlign: 'center' }}>loading available pools...</h1></div>
+        
         if (error) return <h1>App Error!</h1>
-
+        
         if (!poolSelected) return <PoolPicker pools={pools} handlePoolSelect={setPoolSelected} />
         
-        return <WrappedComponent {...props} pools={pools} changePool={setPoolSelected} selectedPool={poolSelected} />
+        return (
+            <>
+                <PoolPicker currentPool={poolSelected} pools={pools} handlePoolSelect={setPoolSelected} />
+                <WrappedComponent {...props} pools={pools} changePool={setPoolSelected} selectedPool={poolSelected} />
+            </>
+        )
     }
 
 export default withPoolSwitching
